9f1f2fa2acd35bd10dfa577e431a9d81a343535e
figueroar <raul.a.figueroa@hotmail.com>
2020-07-02 17:10:41 -0400

    new static index route

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   end
   resources :projects, only: [:index, :new, :create, :update, :delete, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '*other', to: 'static#index'
 end
